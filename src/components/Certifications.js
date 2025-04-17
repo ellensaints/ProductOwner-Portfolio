8daada63628d22db0e605952165151ffa3d3af6a
Ellen <ellencssp@gmail.com>
2025-04-17 18:27:10 -0300

    update images

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -22,14 +22,14 @@ function Certifications() {
         <ul className="certification-list">
           <li>
             <a href="https://gerarcertificado.com.br/validar.php?cert=588e47d28fdb5f4a2f61366d705f9594" target="_blank" rel="noopener noreferrer">
-              <img src="/logo-pm3.jpg" alt="Certificado 1" className="certification-image" />
+              <img src="./logo-pm3.jpg" alt="Certificado 1" className="certification-image" />
               <h3>Product Discovery</h3></a>
               <p>Da Ideação à Realização:</p>
               <p>Uma Jornada de Descoberta</p>
           </li>
           <li>
             <a href="https://gerarcertificado.com.br/validar.php?cert=6c71f5f54d3a8d6e50470fe99c085047" target="_blank" rel="noopener noreferrer">
-              <img src="/logo-pm3.jpg" alt="Certificado 2" className="certification-image" />
+              <img src="./logo-pm3.jpg" alt="Certificado 2" className="certification-image" />
               <h3>Product Management</h3></a>
               <p>Gerenciamento Eficaz:</p>
               <p>Dominando o Ciclo de Vida do Produto</p>
